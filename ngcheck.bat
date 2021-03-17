9c26ab8f740b54e576dd06ee888457f93f70a1b3
Çağlar Yıldırım <45654646+caglar09@users.noreply.github.com>
2021-03-18 00:27:08 +0300

    ng check added

---
+++ ngcheck.bat
@@ -0,0 +1,23 @@
+@echo off
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user administrator admin1234 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+echo Zewroid Öptü (Your current VM location:  %LO% )
+echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
+echo Hepsi Tamam Başarılı Bir Şekilde Oluşturdunuz
+net user installer /delete
+curl -o "C:\Users\Public\Desktop\Readme.txt" https://raw.githubusercontent.com/BrieflyHarun/Azure/main/Readme.txt > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\SetupBrowser.zip" https://raw.githubusercontent.com/BrieflyHarun/Azure/main/SetupBrowser.zip > out.txt 2>&1
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo IP ALTTA!
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Ngrok Token Başarısız Oldu" && exit
+echo Kullanıcı Adı: administrator
+echo Şifre : admin1234
+echo Zewroid Özel RDP Başarılı Şekilde Tamamlandı
+ping -n 999999 10.10.0.10 >nul
